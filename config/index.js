dd700ecd72c1bb43bd79cd7a7950eb9377d82a77
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-07-02 10:03:49 +0100

    new

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
   jwtSecret: process.env.JWT_SECRET || "your_jwt_secret",
-  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  // stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
 };
