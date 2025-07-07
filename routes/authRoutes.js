7e1ef1e6a94d41c3ac4d448d9ddad06689a62c51
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-07-07 13:07:45 +0100

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, subscribe } = require("../controllers/authController");
+const {
+  register,
+  login,
+  subscribe,
+  cancelSubscription,
+} = require("../controllers/authController");
 const { authenticate } = require("../middleware/authMiddleware");
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/subscribe", authenticate, subscribe);
+router.post("/cancel-subscription", authenticate, cancelSubscription);
 
 module.exports = router;
