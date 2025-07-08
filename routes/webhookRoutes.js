fcff5e0fd0c7291509252964b6e92732eea69708
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-07-08 13:56:06 +0100

    Update webhookRoutes.js

--- routes/webhookRoutes.js
+++ routes/webhookRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const webhookController = require("../controllers/webhookController");
 
 router.post("/stripe", webhookController.handleStripeWebhook);
+router.post("/paypal", webhookController.handlePaypalWebhook);
 
 module.exports = router;
