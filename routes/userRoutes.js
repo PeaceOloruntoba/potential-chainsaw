c7044742ad48382d11e90382d2e89af9bc1514d7
PeaceOloruntoba <profprincepeace@gmail.com>
2025-07-20 10:34:59 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const {
-  getOppositeGenderUsers,
   getProfile,
   updateProfile,
   getSingleUserProfileWithPhotos,
 } = require("../controllers/userController");
 const { authenticate } = require("../middleware/authMiddleware");
 
-router.get("/", authenticate, getOppositeGenderUsers);
+router.get("/", authenticate, getSingleUserProfileWithPhotos);
 router.get("/:userId", authenticate, getSingleUserProfileWithPhotos);
 router.get("/profile", authenticate, getProfile);
 router.post("/profile", authenticate, updateProfile);
