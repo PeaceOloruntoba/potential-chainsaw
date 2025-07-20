ed9813af0e64a6727a83da2ccf52474c72053b1f
PeaceOloruntoba <profprincepeace@gmail.com>
2025-07-20 12:35:52 +0100

    Update photoModel.js

--- models/photoModel.js
+++ models/photoModel.js
@@ -1,23 +1,9 @@
-const mongoose = require('mongoose');
+const photoSchemaExample = {
+  _id: "ObjectId",
+  userId: "ObjectId",
+  cloudinaryUrl: "String",
+  cloudinaryPublicId: "String",
+  createdAt: "Date",
+};
 
-const photoSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  cloudinaryUrl: {
-    type: String,
-    required: true,
-  },
-  cloudinaryPublicId: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
-
-module.exports = mongoose.model('Photo', photoSchema);
+module.exports = { photoSchemaExample };
