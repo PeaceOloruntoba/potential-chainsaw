db929da99f6047f4b3285e267dc108f0e07c3730
PeaceOloruntoba <profprincepeace@gmail.com>
2025-07-20 09:37:37 +0100

    Update photoRoutes.js

--- routes/photoRoutes.js
+++ routes/photoRoutes.js
@@ -1,16 +1,18 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const {
-  uploadPhoto,
-  getPhotos,
-  requestPhotoAccess,
-  respondToPhotoRequest,
-} = require("../controllers/photoController");
-const { authenticate } = require("../middleware/authMiddleware");
+const photoController = require('../controllers/photoController');
+const authenticate = require('../middleware/authMiddleware');
+const multer = require('multer');
 
-router.post("/", authenticate, uploadPhoto);
-router.get("/", authenticate, getPhotos);
-router.post("/request", authenticate, requestPhotoAccess);
-router.post("/request/respond", authenticate, respondToPhotoRequest);
+const upload = multer({ dest: 'uploads/' });
+
+router.post('/upload', authenticate, upload.single('image'), photoController.uploadPhoto);
+router.get('/', authenticate, photoController.getUserPhotos);
+router.delete('/:id', authenticate, photoController.deletePhoto);
+
+router.post('/request', authenticate, photoController.requestPhoto);
+router.get('/requests/sent', authenticate, photoController.getSentPhotoRequests);
+router.get('/requests/received', authenticate, photoController.getReceivedPhotoRequests);
+router.put('/requests/:requestId/respond', authenticate, photoController.respondToPhotoRequest);
 
 module.exports = router;
